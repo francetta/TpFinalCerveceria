18502b5baadd8583d52a329f202c0c860f18c715
francetta <97107605+francetta@users.noreply.github.com>
2022-08-21 18:35:09 -0300

    Add files via upload

---
+++ cerveceria.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 21-08-2022 a las 20:12:40
+-- Versión del servidor: 5.7.36
+-- Versión de PHP: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cerveceria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `conciertos`
+--
+
+DROP TABLE IF EXISTS `conciertos`;
+CREATE TABLE IF NOT EXISTS `conciertos` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(250) NOT NULL,
+  `Formacion` text NOT NULL,
+  `Descripcion` text NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `conciertos`
+--
+
+INSERT INTO `conciertos` (`Id`, `Nombre`, `Formacion`, `Descripcion`) VALUES
+(2, 'Jazz trio', 'Cosme Fulanito Guitarra\r\nJuan Perez Contrabajo\r\nCosme Perez Bateria', 'Reviven el repertorio de blablabla');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `Id` int(11) NOT NULL,
+  `usuario` varchar(250) NOT NULL,
+  `Password` varchar(250) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`Id`, `usuario`, `Password`) VALUES
+(2, 'Manuel', '46f8f1e74d36fc278b7f468ffb2f3f9b');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
